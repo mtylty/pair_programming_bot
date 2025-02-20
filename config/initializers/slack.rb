2662bbcaa90abf905909d7c66dfc53664a454dcb
Matteo Latini <matteolatini@nebulab.com>
2025-02-20 14:20:04 +0100

    At this point this is too much of a spike to even think about commit messages

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,4 +1,5 @@
 SlackRubyBotServer.configure do |config|
   config.oauth_version = :v2
   config.oauth_scope = ['chat:write', 'im:write', 'groups:write', 'channels:read', 'users:read']
+  config.database_adapter = :mongoid
 end
